c7ab843274bb83f3addfc8f942dbd91d75d5a4be
Eljan Peralta <eljan233@gmail.com>
2026-08-14 08:24:50 +0800

    feat: enhance Dashboard layout and styling for improved user experience

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,12 +5,13 @@ import heartIcon from "../assets/icons/drawn-heart-icon.png";
 
 export const Dashboard = () => {
   const { user } = useAuth();
+
   return (
     <>
       <Navbar />
       <div className="min-h-screen flex items-center bg-white overflow-y-auto">
-        <div className="flex-1 min-w-0 h-full flex flex-col  items-center text-center">
-          <span className="bg-[#fff5cd] px-3 py-3 rounded-3xl text-[#ff914d]">
+        <div className="flex-1 min-w-0 h-screen flex flex-col items-center text-center justify-center">
+          <span className="bg-[#fff5cd] px-3 py-3 rounded-3xl text-black mb-10">
             <img
               src={heartIcon}
               alt="heart"
@@ -19,7 +20,7 @@ export const Dashboard = () => {
             {user?.name}
           </span>
           <div className="flex flex-col gap-6 items-center text-7xl max-w-175 w-[60%] text-start">
-            <span className="font-bold text-black w-[90%]">
+            <span className="font-bold text-black">
               Every moment we make{" "}
               <span className="text-[#a4544b]">together</span>
             </span>
@@ -27,20 +28,19 @@ export const Dashboard = () => {
               Our bucket lists, photos, milestones, and all the little
               adventures that make us, us.
             </span>
-            <div className="flex flex-col gap-3 self-start text-sm w-full text-[#545454] font-semibold">
-              You are not in a couple yet.
-              <CommonButton text="Join a couple" />
-              <CommonButton text="Create a couple" />
-            </div>
+            {!user?.couple_id && (
+              <div className="flex flex-col gap-3 self-start text-sm w-full text-[#545454] font-semibold">
+                You are not in a couple yet.
+                <CommonButton text="Join a couple" />
+                <CommonButton text="Create a couple" />
+              </div>
+            )}
           </div>
         </div>
 
         <div className="flex-none bg-[#fff5cd] w-[70%] min-w-[320px] flex flex-col justify-center h-screen z-10 items-center">
           <div className="flex flex-col gap-3 items-center">
-            <span className="mt-4 text-black text-base max-w-md text-[24.5px]">
-              Our bucket lists, photos, milestones, and all the little
-              adventures that make us, us.
-            </span>
+            <span className="mt-4 text-black text-base max-w-md text-[24.5px]"></span>
           </div>
         </div>
       </div>
