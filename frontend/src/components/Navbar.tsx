7276ef2c873dea1a29c8bc3b90d8e64f9b1b1117
Eljan Peralta <eljan233@gmail.com>
2026-09-01 09:38:36 +0800

    refactor: restructure Navbar component for improved layout and readability

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,19 +10,26 @@ export const Navbar = () => {
       aria-label="Main navigation"
       className="fixed top-0 left-0 right-0 z-50 border-b bg-white border-black w-full h-12 flex items-center px-4"
     >
-      <Link
-        to={homeTarget}
-        aria-label="Go to home"
-        className="text-black font-semibold text-2xl hover:cursor-pointer"
-      >
-        Dear<span className="text-[#a4544b]">Us</span>
-      </Link>
-      <span
-        className="ml-auto text-sm font-medium text-[#4b2723] hover:cursor-pointer"
-        onClick={logout}
-      >
-        Logout
-      </span>
+      <div className="flex items-center gap-4 w-full justify-between">
+        <span className="text-black font-semibold text-2xl hover:cursor-pointer">
+          Dear<span className="text-[#a4544b]">Us</span>
+        </span>
+
+        <Link
+          to={homeTarget}
+          aria-label="Go to home"
+          className="text-black font-medium text-sm hover:cursor-pointer"
+        >
+          Home
+        </Link>
+
+        <span
+          className=" text-sm font-medium text-[#4b2723] hover:cursor-pointer"
+          onClick={logout}
+        >
+          Logout
+        </span>
+      </div>
     </nav>
   );
 };
