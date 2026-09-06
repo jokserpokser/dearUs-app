a1cfe8b4aea9c0d0e6c6e88f33061d52ac8b1cba
Eljan Peralta <eljan233@gmail.com>
2026-09-06 14:36:07 +0800

    feat: specify dotenv config path in knexfile for environment variable loading

--- backend/src/knexfile.ts
+++ backend/src/knexfile.ts
@@ -1,7 +1,8 @@
 import type { Knex } from "knex";
 import * as dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const config: { [key: string]: Knex.Config } = {
   development: {
